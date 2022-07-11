e505cca5c8373a7fea0bccf71ce867a89af655a4
Fook <59011305+thanawat-ku@users.noreply.github.com>
2022-07-11 14:40:29 +0700

    Update __init__.py

--- hello/__init__.py
+++ hello/__init__.py
@@ -1,22 +1,11 @@
 import check50
-import check50.c
 
 @check50.check()
 def exists():
     """hello.c exists"""
-    check50.exists("hello.c")
+    check50.exists("hello.py")
 
 @check50.check(exists)
-def compiles():
-    """hello.c compiles"""
-    check50.c.compile("hello.c", lcs50=True)
-
-@check50.check(compiles)
-def emma():
-    """responds to name Emma"""
-    check50.run("./hello").stdin("Emma").stdout("Emma").exit()
-
-@check50.check(compiles)
-def rodrigo():
-    """responds to name Rodrigo"""
-    check50.run("./hello").stdin("Rodrigo").stdout("Rodrigo").exit()
+def testhello():
+    """output of hello"""
+    check50.run("python3 heelo.py").stdout("hello").exit()
