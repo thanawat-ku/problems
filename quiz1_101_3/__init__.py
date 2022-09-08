65d0c8505f872e440b3cc0f385516838951359f4
Fook <59011305+thanawat-ku@users.noreply.github.com>
2022-09-08 09:09:26 +0700

    Create __init__.py

---
+++ quiz1_101_3/__init__.py
@@ -0,0 +1,45 @@
+import check50
+from re import escape
+
+
+@check50.check()
+def exists():
+    """water.py exists"""
+    check50.exists("water.py")
+
+
+@check50.check(exists)
+def test_0():
+    """usage unit = 0"""
+    input = "0"
+    output = "0"
+    check50.run("python3 water.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_minus10():
+    """usage unit = -10"""
+    input = "10"
+    output = "Enter water usage unit: "
+    check50.run("python3 water.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).kill()
+
+
+@check50.check(exists)
+def test_5():
+    """usage unit = 5"""
+    input = "5"
+    output = "Cost of water usage: 0 Baht"
+    check50.run("python3 water.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_50():
+    """usage unit = 50"""
+    input = "50"
+    output = "Cost of water usage: 400 Baht"
+    check50.run("python3 coke.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+def regex(answer):
+    """match case-insensitively with only whitespace on either side"""
+    return rf'(?i)^\s*{answer}\s*$'
