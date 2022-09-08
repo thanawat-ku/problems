0b2b09cc17807f8e41a8441431c373cd71595761
Fook <59011305+thanawat-ku@users.noreply.github.com>
2022-09-08 08:51:12 +0700

    Update __init__.py

--- quiz1_101_2/__init__.py
+++ quiz1_101_2/__init__.py
@@ -12,7 +12,7 @@ def exists():
 def test_change():
     """list of student name"""
     output="List of sorted students\nDraco\nHarry\nHermione\nPadma\nRon"
-    check50.run("python3 sort_name.py").stdin("Ron", prompt=True).stdin("Harry", prompt=True).stdin("Herminone", prompt=True).stdin("Draco", prompt=True).stdin("Padma", prompt=True).stdout(regex(output), output, regex=True).exit()
+    check50.run("python3 sort_name.py").stdin("Ron", prompt=True).stdin("Harry", prompt=True).stdin("Hermione", prompt=True).stdin("Draco", prompt=True).stdin("Padma", prompt=True).stdout(regex(output), output, regex=True).exit()
 
 def regex(answer):
     """match case-insensitively with only whitespace on either side"""
