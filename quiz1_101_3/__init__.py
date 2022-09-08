e78dc4ce20de938b7383c38a55b301c78a9a507a
Fook <59011305+thanawat-ku@users.noreply.github.com>
2022-09-08 09:15:49 +0700

    Update __init__.py

--- quiz1_101_3/__init__.py
+++ quiz1_101_3/__init__.py
@@ -12,7 +12,7 @@ def exists():
 def test_0():
     """usage unit = 0"""
     input = "0"
-    output = "0"
+    output = "Cost of water usage: 0 Baht"
     check50.run("python3 water.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
@@ -20,8 +20,7 @@ def test_0():
 def test_minus10():
     """usage unit = -10"""
     input = "10"
-    output = "Enter water usage unit: "
-    check50.run("python3 water.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).kill()
+    check50.run("python3 water.py").stdin(input, prompt=True).stdin(input, prompt=True).stdin(input, prompt=True).kill()
 
 
 @check50.check(exists)
@@ -37,7 +36,7 @@ def test_50():
     """usage unit = 50"""
     input = "50"
     output = "Cost of water usage: 400 Baht"
-    check50.run("python3 coke.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+    check50.run("python3 water.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
 
 
 def regex(answer):
