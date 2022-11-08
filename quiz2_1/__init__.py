6453bbd22c444871b74768aa9bf6683911ab5d1f
Thanawat Phattaraworamet <thanawat.pha@ku.th>
2022-11-08 13:21:17 +0700

    quiz2_1

---
+++ quiz2_1/__init__.py
@@ -0,0 +1,28 @@
+import check50
+from re import escape
+
+
+@check50.check()
+def exists():
+    """weather.py exists"""
+    check50.exists("weather.py")
+
+
+@check50.check(exists)
+def test_sakon():
+    """weather of sakon"""
+    input = "sakon nakhon"
+    output = "lat = 17.3333, lon = 103.8333"
+    check50.run("python3 weather.py").stdin(input, prompt=True).stdout(regex(output), output, regex=True).exit()
+
+
+@check50.check(exists)
+def test_bangkok():
+    """weather of bangkok"""
+    input = "lat = 13.75, lon = 100.5197"
+    check50.run("python3 weather.py").stdin(input, prompt=True).stdin(input, prompt=True).stdin(input, prompt=True).kill()
+
+
+def regex(answer):
+    """match case-insensitively with only whitespace on either side"""
+    return rf'(?i)^\s*{answer}\s*$'
