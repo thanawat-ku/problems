9af89a5f81206cda4c9210ec8017b7e956f20888
Thanawat Phattaraworamet <thanawat.pha@ku.th>
2022-11-08 13:46:15 +0700

    quiz2_2

---
+++ quiz2_2/__init__.py
@@ -0,0 +1,28 @@
+import check50
+from re import escape
+
+
+@check50.check()
+def exists():
+    """score.py exists"""
+    check50.exists("score.py")
+
+
+@check50.check(exists)
+def test_sakon():
+    """input score"""
+    input = "sakon nakhon"
+    check50.run("python3 score.py").stdin("A", prompt=True).stdin("30", prompt=True).stdin("20", prompt=True).stdin("30", prompt=True) \
+    .stdin("B", prompt=True).stdin("20", prompt=True).stdin("10", prompt=True).stdin("30", prompt=True) \
+    .stdin("C", prompt=True).stdin("10", prompt=True).stdin("10", prompt=True).stdin("10", prompt=True) \
+    .stdin("", prompt=True).stdin("0", prompt=True).stdin("0", prompt=True).stdin("0", prompt=True) \
+    .exit()
+
+@check50.check()
+def test_csv():
+    """score.csv exists"""
+    check50.exists("score.csv")
+
+def regex(answer):
+    """match case-insensitively with only whitespace on either side"""
+    return rf'^{answer}.*'
